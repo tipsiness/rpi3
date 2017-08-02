e64cdf80b0d83135cdb3aa8d268afc8eb4203558
tipsiness <tipsiness@gmail.com>
2017-08-02 14:07:25 +0900

    Add files via upload

---
+++ RGBLed/leds.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request
+import RPi.GPIO as GPIO
+
+app = Flask(__name__)
+GPIO.setmode(GPIO.BCM)
+
+leds = {
+    23: {'name' : 'Red LED', 'state' : GPIO.LOW},
+    24: {'name' : 'Green LED', 'state' : GPIO.LOW},
+    25: {'name' : 'Blue LED', 'state' : GPIO.LOW}
+}
+
+for led in leds:
+    GPIO.setup(led, GPIO.OUT)
+    GPIO.output(led, GPIO.LOW)
+
+def getGpioState():
+    for led in leds:
+        leds[led]['state'] = GPIO.input(led)
+
+    return leds
+
+@app.route("/")
+def main():
+    gpioState = {
+        'leds' : getGpioState()
+    }
+
+    return render_template('main.html', **gpioState)
+
+@app.route("/<led>/<act>")
+def action(led, act):
+    led = int(led)
+    leds = getGpioState()
+    dev = leds[led]['name']
+
+    if act == "on":
+        GPIO.output(led, GPIO.HIGH)
+        msg = "Truned " + dev + " on."
+    elif act == "off":
+        GPIO.output(led, GPIO.LOW)
+        msg = "Truned " + dev + " off."
+    elif act == "toggle":
+        GPIO.output(led, not GPIO.input(led))
+        msg = "Toggled " + dev + "."    
+    else:
+        msg = "Undefined action!"
+
+    gpioState = {
+        'msg' : msg,
+        'leds' : getGpioState()
+    }
+
+    return render_template('main.html', **gpioState)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8888, debug=True)
+
