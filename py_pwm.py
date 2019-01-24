5ce9da983fd25ac141b9cfb661ff52c211f27466
tipsiness <tipsiness@gmail.com>
2019-01-24 09:30:00 +0900

    Create py_pwm.py

---
+++ py_pwm.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+RANGE = 60
+LED = 12
+
+GPIO.setup(LED, GPIO.OUT)
+LED = GPIO.PWM(LED, RANGE)
+
+LED.start(0)
+delay = 0.05
+
+try:
+	while True:
+		for i in range(0, RANGE+1):
+			LED.ChangeDutyCycle(i)
+			time.sleep(delay)
+		for i in range(RANGE, -1, -1):
+			LED.ChangeDutyCycle(i)
+			time.sleep(delay)
+
+except KeyboardInterrupt:
+	LED.stop()
+	GPIO.cleanup()
+
