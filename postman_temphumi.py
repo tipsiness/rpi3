be0864acb68910a1d5a86f66edd693f44d9b18c3
tipsiness <tipsiness@gmail.com>
2019-01-25 16:09:34 +0900

    Update postman_temphumi.py

--- postman_temphumi.py
+++ postman_temphumi.py
@@ -7,7 +7,7 @@ url = "https://api.artik.cloud/v1.1/messages"
 
 headers = {
 	'Content-Type': "application/json",
-	'Authorization': "Bearer 371cbd04bffb495daa398f8d6940a398",
+	'Authorization': "Bearer YOUR_TOKEN",
 	'cache-control': "no-cache",
 	'Postman-Token': "1502b7e6-12b7-410b-b5b9-0046e0a06972"
 }
@@ -16,7 +16,7 @@ try:
 	while True:
 		humi, temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 4)
 
-		payload = "{\"data\": {\"temperature\": " +str(temp)+  ",\"humidity\": " +str(humi)+ "},\"sdid\": \"4f550ef3656c474ba755c763f91698f5\", \"type\": \"message\"}"
+		payload = "{\"data\": {\"temperature\": " +str(temp)+  ",\"humidity\": " +str(humi)+ "},\"sdid\": \"YOUR_DEVICE_ID\", \"type\": \"message\"}"
 
 		response = requests.request("POST", url, data=payload, headers=headers)
 		print(response.text)
