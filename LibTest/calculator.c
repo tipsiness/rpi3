41e521e8a64939c254e807b49d5e996deb71f3fa
tipsiness <tipsiness@gmail.com>
2019-01-29 16:21:34 +0900

    Add files via upload

---
+++ LibTest/calculator.c
@@ -0,0 +1,17 @@
+/* calculator.c */
+int add(int x, int y) { 
+	return (x + y);
+}
+
+int sub(int x, int y) { 
+	return (x – y);
+}
+
+int mul(int x, int y) { 
+	return (x * y);
+}
+
+float div(int x, int y) { 
+	return (x / y);
+}
+
---
+++ LibTest/calculator.h
@@ -0,0 +1,11 @@
+/* calculator.h */
+#ifndef __CALCULATOR_H__
+#define __CALCULATOR_H__
+
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+float div(int, int);
+
+#endif
+
