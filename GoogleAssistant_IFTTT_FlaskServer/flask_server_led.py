7fbf948cb2656a662c8f08e5b2aca5de21d4c0ec
tipsiness <tipsiness@gmail.com>
2020-01-10 11:10:29 +0900

    Create flask_server_led.py

---
+++ GoogleAssistant_IFTTT_FlaskServer/flask_server_led.py
@@ -0,0 +1,34 @@
+from flask import Flask, redirect, url_for, request, render_template, jsonify
+import json
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def main():
+  data = request.get_json(silent=True)
+	resp = json.dumps(data)
+	values = json.loads(resp)
+
+	print(resp)
+
+  state = values['state']
+  print "Device State: ", state
+    
+  stateValue = 0
+  if state == 'on':
+    stateValue = 1
+	  GPIO.output(11, GPIO.HIGH)
+		print "Lamp is turned on."
+  elif state == 'off':
+    stateValue = 0
+		GPIO.output(11, GPIO.LOW)
+		print "Lamp is turned off."
+    
+	return resp
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8888, debug=True)
